2a8bb53238d1c03dba17bd61d1b683c910f3a4c6
bukinoshita <bukinoshita@gmail.com>
2020-06-03 19:56:23 -0300

    feat(layouts): add meta

--- layouts/page/page.tsx
+++ layouts/page/page.tsx
@@ -1,39 +1,70 @@
-import React, {ReactNode} from 'react'
+import React, { ReactNode } from 'react'
+import Head from 'next/head'
 
-import {Colors} from 'ui/theme'
+import pkg from '../../package.json'
+
+import { Colors } from 'ui/theme'
 
 export type PageProps = {
-	children: ReactNode
+  children: ReactNode
 }
 
-export const Page = ({children}: PageProps) => {
-	return (
-		<>
-			{children}
+export const Page = ({ children }: PageProps) => {
+  return (
+    <>
+      <Head>
+        <title>{pkg.name}</title>
+        <meta name="description" content={pkg.description} />
 
-			<style jsx global>{`
-				* {
-					margin: 0;
-					padding: 0;
-					box-sizing: border-box;
-					-webkit-font-smoothing: antialiased;
-				}
+        <link rel="apple-touch-icon" href="static/icon.png" />
+        <link rel="icon" href="static/icon.png" type="image/png" />
+        <meta name="theme-color" content={Colors.Background} />
 
-				body {
-					font-family: 'Inter';
-					background-color: ${Colors.Background};
-					color: ${Colors.White};
-				}
+        <meta name="viewport" content="width=device-width, user-scalable=no" />
 
-				svg {
-					vertical-align: middle;
-				}
+        <link
+          href="https://fonts.googleapis.com/css?family=Inter:500&display=swap"
+          rel="stylesheet"
+        />
 
-				img {
-					max-width: 100%;
-					width: 100%;
-				}
-			`}</style>
-		</>
-	)
+        <meta property="og:title" content={pkg.name} />
+        <meta property="og:description" content={pkg.description} />
+
+        <meta name="twitter:image" content="https://naodigaisso.com/static/cover.png" />
+        <meta name="twitter:card" content="summary_large_image" />
+
+        <meta property="og:title" content={pkg.name} />
+        <meta property="og:description" content={pkg.description} />
+        <meta property="og:image" content="https://naodigaisso.com/static/cover.png" />
+        <meta property="og:url" content="https://naodigaisso.com" />
+        <meta property="og:type" content="website" />
+      </Head>
+
+      {children}
+
+      <style jsx global>{`
+        * {
+          margin: 0;
+          padding: 0;
+          box-sizing: border-box;
+          -webkit-font-smoothing: antialiased;
+        }
+
+        body {
+          font-family: 'Inter';
+          background-color: ${Colors.Background};
+          color: ${Colors.White};
+        }
+
+        svg {
+          vertical-align: middle;
+        }
+
+        img {
+          max-width: 100%;
+          width: 100%;
+        }
+      `}</style>
+    </>
+  )
 }
