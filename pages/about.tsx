a374610f81287c659d5450ced411d45c08a67457
bukinoshita <bukinoshita@gmail.com>
2020-12-19 20:58:07 -0300

    refactor: tailwind

--- pages/about.tsx
+++ pages/about.tsx
@@ -6,26 +6,29 @@ import { space, Colors } from 'ui/theme'
 export const About = () => {
   return (
     <Page style={{ justifyContent: 'initial' }}>
-      <section>
-        <h1>Sobre o projeto ndi</h1>
+      <section className="mx-auto my-10 max-w-screen-sm">
+        <h1 className="text-4xl mb-8">Sobre o projeto ndi</h1>
 
-        <p>
+        <p className="text-gray-200 mb-5 text-lg leading-8">
           O projeto ndi é um projeto brasileiro para criar consciência em
           palavras e expressões que usamos no nosso dia a dia que são racistas
           e/ou sexistas.
         </p>
 
-        <p>
+        <p className="text-gray-200 mb-5 text-lg leading-8">
           Nós tentamos concientizar e mostrar o porque da palavra ou expressão
           usada é racista ou sexista, e também mostramos uma melhor maneira de
           se expressar substituindo a palavra.
         </p>
 
-        <h2>Quero ajudar a contribuir e melhorar</h2>
+        <h2 className="mt-10 mb-5 text-2xl">
+          Quero ajudar a contribuir e melhorar
+        </h2>
 
-        <p>
+        <p className="text-gray-200 mb-5 text-lg leading-8">
           Somos um projeto Open Source, o código do projeto está disponível no{' '}
           <a
+            className="text-blue-700 hover:underline"
             href="https://github.com/bukinoshita/naodigaisso/blob/main/contributing.md"
             rel="noopener noreferrer"
           >
@@ -35,27 +38,30 @@ export const About = () => {
           novas features e bugs.
         </p>
 
-        <hr />
+        <hr className="border-t border-gray-100 mx-auto mt-6 mb-7" />
 
-        <p>
+        <p className="text-gray-200 mb-5 text-lg leading-8">
           Nós temos bastante cuidado ao adicionar uma nova palavra ou expressão,
           mas o ndi pode conter palavras ou expressões incorretas. Estamos
           sempre dispostos a aprender e melhorar qualquer expressão mal
           utilizada.
         </p>
 
-        <p>
+        <p className="text-gray-200 mb-5 text-lg leading-8">
           Se você viu algum bug ou alguma expressão sendo mal utilizada, por
           favor entre em contato com a gente criando um ticket no nosso{' '}
-          <a href="https://github.com/bukinoshita/naodigaisso/issues/new?template=1.Bug_report.md">
+          <a
+            className="text-blue-700 hover:underline"
+            href="https://github.com/bukinoshita/naodigaisso/issues/new?template=1.Bug_report.md"
+          >
             GitHub
           </a>
           .
         </p>
 
-        <hr />
+        <hr className="border-t border-gray-100 mx-auto mt-6 mb-7" />
 
-        <p>
+        <p className="text-gray-200 mb-5 text-lg leading-8">
           Você pode ajudar a gente a sugerir novas features, reportando
           expressões mal utilizadas e bugs, adicionando novas expressões e
           compartilhando nosso projeto.
@@ -70,37 +76,7 @@ export const About = () => {
       </section>
 
       <style jsx>{`
-        section {
-          max-width: 600px;
-          margin-left: auto;
-          margin-right: auto;
-          margin-top: ${space.spacing(10)};
-        }
-
-        h1 {
-          font-size: 32px;
-          margin-bottom: ${space.spacing(8)};
-        }
-
-        h2 {
-          font-size: 24px;
-          margin-top: ${space.spacing(8)};
-          margin-bottom: ${space.spacing(5)};
-        }
-
-        a {
-          color: ${Colors.Primary};
-        }
-
-        p {
-          font-size: 18px;
-          line-height: 1.75;
-          margin-bottom: ${space.spacing(5)};
-        }
-
         hr {
-          border-top: 1px solid ${Colors.GrayLighter};
-          margin: ${space.spacing(6)} auto ${space.spacing(7)};
           max-width: 75px;
         }
       `}</style>
